b024ae235044d8fc3f6a404f0194088932b16898
Rinorragi <rinorragi@gmail.com>
2024-05-28 10:50:51 +0300

    Add simple logic to fetch skulls and honor

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# rumblescraper
+# Rumble scraper
+
+Small utility to scrape guild details from warcraftrumble.gg. Zero guarantees for working.
+
+## Setup
+
+You need .NET SDK to run this. [Download .NET 8 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/8.0).
+
+## How to run
+
+On the directory where the script is run following with 123456789 of being your guild id.
+
+```powershell
+dotnet fsi .\scrape_goblins.fsx 123456789
+```
+
+## How to read data
+
+The software creates `/data` directory and stores the results in txt file. You can run e.g. `cat .\data\123456789.txt` to read the data or open it with text editor.
---
+++ scrape_goblins.fsx
@@ -0,0 +1,68 @@
+// Imports
+open System.IO
+open System.Net.Http
+open System.Net.Http.Json
+
+// Arguments for script, skip the filneme
+let args : string array = fsi.CommandLineArgs |> Array.tail
+let guildId = args[0]
+let tabSeparator = '\t' // Used for copypaste, might be \t or ; or ,
+let dataPath = Path.Combine(".","data")
+let guildFilePath = Path.Combine(dataPath, (sprintf "%s.txt" guildId))
+
+// Full models, but we wont use everything
+type Goblin = {
+    id : int
+    name : string
+    btag : string
+    skulls : int
+    honor : int
+    avatar : string
+    lastUpdate : int64
+}
+
+type Guild = {
+    id : int
+    name : string 
+    member_count : int
+    description : string
+    members : Goblin list
+}
+
+type Data = {
+    data : Guild
+    token : string 
+    link : string
+}
+
+let ensureDataDirectory = 
+    Directory.CreateDirectory(dataPath) |> ignore
+
+#nowarn "3511" // Disable whining about dynamic compilation
+let updateGoblinData = 
+    task {
+        use client = new HttpClient()
+        let url = $"https://api.warcraftrumble.gg/guild/{guildId}"
+        let! guildResult = client.GetFromJsonAsync<Data>(url)
+        
+        let goblins = 
+            guildResult.data.members
+            |> List.sortBy (fun (gobo : Goblin) -> gobo.name.ToLower()) // Sort by lowercase, because of sheets 
+            |> List.fold (fun (allGoblinsString : string) (gobo : Goblin) ->
+                let goboString = sprintf "%s%c%d%c%d" gobo.name tabSeparator gobo.skulls tabSeparator gobo.honor
+                sprintf "%s%s%s" allGoblinsString goboString System.Environment.NewLine
+            ) ""
+        let file = File.CreateText(guildFilePath)
+        file.Write(goblins.ToCharArray())
+        file.Close()
+
+        printfn "%s" guildFilePath
+    }
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
+// Create data directory if it does not exists
+ensureDataDirectory
+// Fetch goblin data from internets to directory
+updateGoblinData 
+
